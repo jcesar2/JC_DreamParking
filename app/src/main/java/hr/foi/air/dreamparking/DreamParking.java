097e561041b16054c39750f2293f3f557e7676a5
ninozvorc <nino.zvorc@foi.hr>
2015-10-13 16:02:38 +0200

    Initial commit

---
+++ app/src/main/java/hr/foi/air/dreamparking/DreamParking.java
@@ -0,0 +1,13 @@
+package hr.foi.air.dreamparking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DreamParking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dream_parking);
+    }
+}
