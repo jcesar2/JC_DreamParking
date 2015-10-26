b94845d3f1c3ce3416123a419e96b18d3ea2cb56
jcesar2 <josip.cesar2@foi.hr>
2015-10-27 00:46:03 +0100

    Kreiranje izbornika

--- app/src/main/java/hr/foi/air/dreamparking/DreamParking.java
+++ app/src/main/java/hr/foi/air/dreamparking/DreamParking.java
@@ -2,6 +2,9 @@ package hr.foi.air.dreamparking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class DreamParking extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class DreamParking extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dream_parking);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        getMenuInflater().inflate(R.menu.main, menu);
+        return  true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        switch(id)
+        {
+            case R.id.action_search:
+                break;
+            case R.id.action_settings:
+                break;
+        }
+        Toast.makeText(this, "Menu item "+ item.getTitle()+ " clicked!", Toast.LENGTH_SHORT).show();
+        return true;
     }
 }
